cad92db484253815d82a0bbb8df3d617c8f6bd0e
RGGU0 <89143056+RGGU0@users.noreply.github.com>
2023-09-26 23:56:14 +0500

    Add files via upload
    2 часть

--- practice/01 Basics/modules/metrics.py
+++ practice/01 Basics/modules/metrics.py
@@ -50,7 +50,15 @@ def norm_ED_distance(ts1, ts2):
 
     # INSERT YOUR CODE 
 
-    norm_ed_dist = (sum((ts1 - ts2) ** 2)) ** 0.5
+    n = np.shape(ts1)[0]
+
+    ad1 = sum(ts1) / n
+    ad2 = sum(ts2) / n
+
+    sd1 = (sum(ts1 ** 2) / n - ad1 ** 2) ** 0.5
+    sd2 = (sum(ts2 ** 2) / n - ad2 ** 2) ** 0.5
+
+    norm_ed_dist = (2 * n *(1 - (np.dot(ts1,ts2) - n * ad1 * ad2) / (n * sd1 * sd2))) ** 0.5
 
     return norm_ed_dist
 
@@ -81,12 +89,13 @@ def DTW_distance(ts1, ts2, r=None):
     # INSERT YOUR CODE
 
     m = len(ts1)
+    r = r*m
     D = np.zeros((m+1,m+1))
     D[0,:] = np.inf
     D[:,0] = np.inf
     D[0,0] = 0
     for i in range(0,m) :
-      for j in range(0,m):
+      for j in range(max(0,int(i-r)),min(m,int(i+r))):
         d = (ts1[i] - ts2[j]) ** 2
         D[i+1,j+1] = d + min(D[i,j+1],D[i+1,j],D[i,j])
     dtw_dist = D[m ,m ]
